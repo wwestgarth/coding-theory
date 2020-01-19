b95910f445026bc883e58c863b2c813f9a2f8167
William Westgarth <william.westgarth@ncipher.com>
2020-01-19 22:07:01 +0000

    Create forced arbitrary hammin distance

--- coding/hamming.py
+++ coding/hamming.py
@@ -35,33 +35,70 @@ class Codex:
         return "{0:b}".format(dec)
         
     def __init__(self, basis):
+        # Store basis
         self.basis = basis[:]
         
         # Calculate bit length
         self.max_bit_len = len( self.decimal_to_binary(len(basis) -1) )
         
     def bits_to_data(self, bits):
+        """
+        Convert given bits generated from `basis' back into their true value
+        """
+        
         position = int(bits,2)
         return self.basis[position]
         
     def data_to_bits(self, data):
+        """
+        Convert the given vectory from the basis into a bit-representations
+        """
         position = self.basis.index(data)
         return self.decimal_to_binary(position).rjust(self.max_bit_len,'0')
     
 class Code:
+    """
+    Class to bit-ify a stream from a given basis into a code which
+    """
     
-    def __init__(self, basis):
+    def _expand(self, bits):
+        """
+        Expand each bit to achieve desired hamming distance
+        """
+        return "".join([x*self.hamming_distance for x in bits])
+        
+    def _reduce(self, bits):
+        """
+        Reduce all expanded bits to restore original bit-basis
+        """
+        reduced = []
+        for i in range(0, len(bits), self.hamming_distance):
+            reduced.append( bits[i] )
+        return "".join(reduced)
+    
+    def __init__(self, basis, hamming_distance=1):
+    
+        if hamming_distance < 1:
+            raise ValueError
+            
         self.codex = Codex(basis)
+        self.hamming_distance=hamming_distance
+        
             
     def encode(self, input_steam):
-        
+        """
+        Convert the given stream into bits with defined hamming-distance
+        """
         bit_stream = [ self.codex.data_to_bits(x) for x in input_steam ]
-        
+        bit_stream = [ self._expand(x) for x in bit_stream]
         return "".join(bit_stream)
         
     def decode(self, bit_steam):
-        
-        split_stream = wrap(bit_steam, self.codex.max_bit_len)
+        """
+        Convert the given bit_stream back into its original form
+        """
+        split_stream = wrap(bit_steam, self.hamming_distance*self.codex.max_bit_len)
+        split_stream = [self._reduce(x) for x in split_stream]
         return [self.codex.bits_to_data(x) for x in split_stream]
     
         
--- test/test_hamming.py
+++ test/test_hamming.py
@@ -1,4 +1,5 @@
 import pytest
+from itertools import product
 from coding import hamming
 
 
@@ -10,11 +11,34 @@ def test_hamming_distance(test_input, expected):
 @pytest.mark.parametrize("test_input, expected", [("000000", 0), ("11111", 5), ("10001", 2)])
 def test_hamming_weight(test_input, expected):
     assert hamming.weight(test_input) == expected
-	
+    
 @pytest.mark.parametrize("test_input", [[1,2,3,4], ["duck", "Goose", "Horse"], range(12,22)])
 def test_code_pairwise_checks(test_input):
 
-	code = hamming.Code(test_input)
-	decoded = code.decode(code.encode(test_input))
-	for pair in zip(test_input, decoded):
-		assert pair[0] == pair[1] 
+    code = hamming.Code(test_input, hamming_distance=3)
+    decoded = code.decode(code.encode(test_input))
+    for pair in zip(test_input, decoded):
+        assert pair[0] == pair[1] 
+        
+        
+@pytest.mark.parametrize("test_input, expected", [("00","000000"), ("10","111000")])
+def test_codex_expansion(test_input, expected):
+
+    code = hamming.Code( [1,2,3,4], hamming_distance=3)
+    expanded = code._expand(test_input)
+    assert expanded == expected 
+    assert test_input == code._reduce(expanded)
+        
+        
+@pytest.mark.parametrize("test_input", [1,2,3,4])
+def test_code_forced_hamming(test_input):
+
+    basis = ["1","2","3","4"]
+    code = hamming.Code(basis, hamming_distance=test_input)
+    code_basis = [code.encode(x) for x in basis]
+
+    cartesian_prod = [ x for x in product(code_basis, code_basis) if x[0] != x[1] ]
+    for pair in cartesian_prod:
+        assert hamming.distance(pair[0],pair[1]) >= test_input
+    
+    
